f7abcb56a96c51d925229a348637cef77cdd978b
Mohammed Riyaz <mohammedriyaz.mdr@gmail.com>
2018-04-19 03:36:41 +0530

    Debug

--- app.rb
+++ app.rb
@@ -9,6 +9,11 @@ class Application < Sinatra::Base
     $GlobalHash.to_json
   end
 
+  get '/replica-values' do
+    content_type :json
+    $GlobalReplicas.to_json
+  end
+
   get '/get/:key' do
     resp = $GlobalHash[params[:key]]
     resp = Communicate.fetch_from_replica(params[:key]) if resp.nil?
